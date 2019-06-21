f9105f252612c549de277d6800ccd1055068d889
ferran.coma <ferran.coma@king.com>
2019-06-21 12:54:35 +0200

    filling vectors and initializing

--- Exercises/Exercises/Source/StdAlgorithms/CStdGenerateAndManipulateCollections.cpp
+++ Exercises/Exercises/Source/StdAlgorithms/CStdGenerateAndManipulateCollections.cpp
@@ -1,12 +1,113 @@
 #include "CStdGenerateAndManipulateCollections.h"
 
-CStdGenerateAndManipulateCollections::CStdGenerateAndManipulateCollections()
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+
+CStdGenerateAndManipulateCollections::CStdGenerateAndManipulateCollections() :
+	a{3 , 6, 1, 0, -2, 5 }
 {
 
 }
 
-void CStdGenerateAndManipulateCollections::main()
+void CStdGenerateAndManipulateCollections::main() 
 {
-	
+	Copy();
+	Move();
+	Removing();
+	CreatingCollections();
+}
+
+void CStdGenerateAndManipulateCollections::Copy()
+{
+	std::cout << "COPY / COPY_IF / COPY_N / COPY_BACKWARD" << std::endl;
+
+	std::vector<int> v2(a.size());
+
+	//Copy the elements from one container to the other
+	//Do NOT use if you want to copy the full container
+	copy(begin(a), end(a), begin(v2));
+
+	//Use this to copy full container
+	auto v3 = a;
+
+	auto position = find(begin(a), end(a), 1);
+	std::vector<int> v4(a.size());
+	copy(begin(a), position, begin(v4));
+
+	std::vector<int> v5(a.size());
+	//Copy only the elements that returns true in lambda
+	copy_if(begin(a), end(a), begin(v5), [](int elem) {return elem % 2 == 0; });
+
+	//Copy the N elements from starting on iterator into the other iterator
+	copy_n(begin(v5), 3, begin(v2));
+
+	copy(begin(a), position + 1, position + 1);
+
+	copy_backward(begin(v3), end(v3), end(a));
+
+	//The commented line might work or not depending on the compiler, as you are modifying the same container
+	//copy(begin(a), end(a) - 1, end(a) + 1);
+
+	//This is how we have to do, this, starts from the last element
+	copy_backward(begin(a), end(a) - 1, end(a));
+
+	//Use copy_backword if the 3rd parameter iterator is in the middle of first and second parameter else use copy
+}
+
+void CStdGenerateAndManipulateCollections::Move()
+{
+	std::cout << "MOVE / MOVE_BACKWARD" << std::endl;
+
+	struct test
+	{
+		int a;
+		int b;
+		test() : a(1), b(2) {}
+	};
+
+	std::vector<test> vecA(3);
+	std::vector<test> vecB(1);
+
+	vecB = vecA;
+	vecA[0].a = 5;
+
+	move(begin(vecA), end(vecA), begin(vecB));
+}
+
+void CStdGenerateAndManipulateCollections::Removing()
+{
+	std::cout << "REMOVE / REMOVE_IF" << std::endl;
+
+	auto newend = remove(begin(a), end(a), 3);
+	int s = a.size();
+	int logicalsize = newend - begin(a);
+	a.erase(newend, end(a));
+
+	a = { 3 , 6, 1, 0, -2, 5 };
+	a.erase(remove(begin(a), end(a), 3), end(a));
+}
+
+void CStdGenerateAndManipulateCollections::CreatingCollections()
+{
+	std::vector<int> v6(10);
+	//fill the vector between the iterators
+	fill(begin(v6), end(v6), 1);
+
+	//this will fill from iterator N times
+	fill_n(begin(v6), 6, 2);
+
+	//This one need numeric include
+	//this will set the value N, N+1, N+2... in the range of the iterators
+	iota(begin(v6), end(v6), 5);
+
+	int index = 10;
+	//this will fill the vector with the number generated into the lambda
+	generate(begin(v6), end(v6), [&index]() {return --index; });
+	a = v6;
+	index = 1;
+	//this will fill the vector with the number generated into the lambda, but from begin to N times
+	generate_n(begin(v6), 7, [&index]() {return (index *= 2); });
+
 }
 
--- Exercises/Exercises/Source/StdAlgorithms/CStdGenerateAndManipulateCollections.h
+++ Exercises/Exercises/Source/StdAlgorithms/CStdGenerateAndManipulateCollections.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <vector>
+
 class CStdGenerateAndManipulateCollections
 {
 public:
 	CStdGenerateAndManipulateCollections();
 	void main();
+	void Copy();
+	void Move();
+	void Removing();
+	void CreatingCollections();
+
+private:
+	std::vector<int> a;
 };
