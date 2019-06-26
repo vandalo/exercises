dafa4b23bbf70f23fbe08d8628aeaf9664aae683
ferran.coma <ferran.coma@king.com>
2019-06-26 10:33:58 +0200

    add comments

--- Exercises/Exercises/Source/Templates/CMyTemplateTest.h
+++ Exercises/Exercises/Source/Templates/CMyTemplateTest.h
@@ -1,8 +1,11 @@
 #pragma once
 
+//Variable template specialization
+//By default the maxValue will be 1000 but if we use it as double the value will be 2000 and if we use char, then will be Z
 template <typename T>
 constexpr T maxValue = T(1000);
 
+//This is marked as error by the editor but it's a known bug from Visual studio
 template <>
 constexpr double maxValue<double> = 2000;
 
